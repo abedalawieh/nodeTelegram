012ac7b197b257944cfbc9e441cb5f7b72a0615a
abedalawieh <abedalaweye@gmail.com>
2023-10-03 16:08:33 +0300

    TRYCATCH

--- botActions/firstStart.js
+++ botActions/firstStart.js
@@ -3,16 +3,24 @@ import { firstMessage } from "../messages.js";
 import { firstKeyboard } from "../keyboards.js";
 
 export const firstStart = () => {
-  const photo = "../assets/social-06.jpg";
-  bot.on("message", (msg) => {
-    console.log(msg.message_id);
-    const chatId = msg.chat.id;
-    console.log(msg);
+  try {
+    const photo = "../assets/social-06.jpg";
+    bot.on("message", async (msg) => {
+      console.log(msg.message_id);
+      const chatId = msg.chat.id;
+      console.log(msg);
 
-    bot.sendMessage(chatId, firstMessage, {
-      reply_markup: JSON.stringify(firstKeyboard),
+      try {
+        await bot.sendMessage(chatId, firstMessage, {
+          reply_markup: JSON.stringify(firstKeyboard),
+        });
+        await bot.sendPhoto(chatId, photo);
+        // await bot.sendVideo(chatId, videoFilePath);
+      } catch (error) {
+        console.error("Error sending messages:", error);
+      }
     });
-    bot.sendPhoto(chatId, photo);
-    // bot.sendVideo(chatId, videoFilePath);
-  });
+  } catch (e) {
+    console.error("Error in firstStart:", e);
+  }
 };
--- botActions/handleCallBacksLogin.js
+++ botActions/handleCallBacksLogin.js
@@ -1,46 +1,42 @@
 import bot from "../botCreation.js";
 import { loginKeyboard, firstKeyboard, cancelKey } from "../keyboards.js";
 import { firstMessage } from "../messages.js";
+
 export const callbackQuery = () => {
-  bot.on("callback_query", (query) => {
-    console.log(query);
-    const data = query.data;
-    const chatId = query.message.chat.id;
-    const message_id = query.message.message_id;
-    console.log(message_id);
+  try {
+    bot.on("callback_query", async (query) => {
+      console.log(query);
+      const data = query.data;
+      const chatId = query.message.chat.id;
+      const message_id = query.message.message_id;
+      console.log(message_id);
 
-    if (data === "login") {
-      const text = "Choose a way to Login In";
-      bot.editMessageText(text, {
-        chat_id: chatId,
-        message_id: message_id,
-        reply_markup: JSON.stringify(loginKeyboard),
-      });
-    } else if (data === "loginApi") {
-      const text =
-        "Please enter the API key you received on the site\n If you are not registered, follow this link to register.";
+      if (data === "login") {
+        const text = "Choose a way to Login In";
+        await bot.editMessageText(text, {
+          chat_id: chatId,
+          message_id: message_id,
+          reply_markup: JSON.stringify(loginKeyboard),
+        });
+      } else if (data === "loginApi") {
+        const text =
+          "Please enter the API key you received on the site\n If you are not registered, follow this link to register.";
 
-      bot.editMessageText(text, {
-        chat_id: chatId,
-        message_id: message_id,
-        reply_markup: JSON.stringify(cancelKey), // Return to the previous menu
-      });
-    } else if (data === "goBack1") {
-      // Handle the "Go Back" button
-      const text = firstMessage;
-      bot.editMessageText(text, {
-        chat_id: chatId,
-        message_id: message_id,
-        reply_markup: JSON.stringify(firstKeyboard), // Return to the previous menu
-      });
-    } else if (data === "cancel") {
-      // Handle the "Go Back" button
-      const text = firstMessage;
-      bot.editMessageText(text, {
-        chat_id: chatId,
-        message_id: message_id,
-        reply_markup: JSON.stringify(firstKeyboard), // Return to the previous menu
-      });
-    }
-  });
+        await bot.editMessageText(text, {
+          chat_id: chatId,
+          message_id: message_id,
+          reply_markup: JSON.stringify(cancelKey),
+        });
+      } else if (data === "goBack1" || data === "cancel") {
+        const text = firstMessage;
+        await bot.editMessageText(text, {
+          chat_id: chatId,
+          message_id: message_id,
+          reply_markup: JSON.stringify(firstKeyboard),
+        });
+      }
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
 };
