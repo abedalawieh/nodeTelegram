f9f046b550d7dbe93364791e32b3f7d17fa30fca
abedalawieh <124059093+abedalawieh@users.noreply.github.com>
2023-10-03 14:16:10 +0300

    Update firstStart.js

--- botActions/firstStart.js
+++ botActions/firstStart.js
@@ -3,7 +3,7 @@ import { firstMessage } from "../messages.js";
 import { firstKeyboard } from "../keyboards.js";
 
 export const firstStart = () => {
-  const photo = "https://github.com/abedalawieh/nodeTelegram/blob/main/assets/social-06.jpg";
+  const photo = "https://raw.githubusercontent.com/abedalawieh/nodeTelegram/main/assets/social-06.jpg";
   bot.on("message", (msg) => {
     console.log(msg.message_id);
     const chatId = msg.chat.id;
